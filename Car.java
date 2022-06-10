b406682b2cf7962de5b1de123fdb59f9f99f8839
AnhHQ98 <99629266+AnhHQ98@users.noreply.github.com>
2022-06-10 13:48:05 +0700

    Upload OOP-Car
    Upload OOP-Car - AnhHQ98

---
+++ Car.java
@@ -0,0 +1,39 @@
+class Car{
+    private String name;
+    private Engine engine;
+    private Wheel wheel;
+    Car(String name, Engine engine, Wheel wheel){
+        this.name = name;
+        this.engine = engine;
+        this.wheel = wheel;
+    }
+    public void display(){
+        System.out.println("Car: " + this.name);
+            this.engine.display();
+            this.wheel.display();
+    }
+}
+class Engine{
+    private String name;
+    Engine(String name){
+        this.name = name;
+    }
+    public void display(){
+        System.out.println("Engine: " + name);
+    }
+}
+class Wheel{
+    private String name;
+    Wheel(String name){
+        this.name = name;
+    }
+    public void display(){
+        System.out.println("Wheel: " + name);
+    }
+}
+public class Car{
+    public class void main(String[] args){
+        Car superCar = new Car("Volvo", new Engine("Diesel"), new Wheel("Circle"));
+        superCar.display();
+    }
+}
